8d2ddf69e37bcb8c322d0c4a4ff8f7ea3442a47c
MohamedHamdy2020 <MohamedHamdy2020@bitbucket.org>
2021-05-08 01:19:28 +0200

    add  MovieCard Component

---
+++ src/Components/MovieCard/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  ImageSourcePropType,
+} from 'react-native';
+import styles from './styles';
+
+interface CardProps {
+  image: ImageSourcePropType;
+  title: String;
+  release_date: String;
+  vote_average: String;
+  geners: Array<Gener>;
+}
+
+interface Gener {
+  id: String;
+  title: String;
+}
+
+export const MovieCard: React.FC<CardProps> = ({
+  image,
+  title,
+  release_date,
+  vote_average,
+  geners,
+}: CardProps) => {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.7}>
+      <Image source={image} resizeMode="cover" style={styles.image} />
+      <View style={styles.details}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={[styles.title, styles.date]}>{release_date}</Text>
+
+        <FlatList
+          scrollEnabled={false}
+          data={geners}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => {
+            return (
+              <View style={styles.generView}>
+                <Text style={styles.gener} numberOfLines={1}>
+                  {item.title}
+                </Text>
+              </View>
+            );
+          }}
+          numColumns={2}
+        />
+      </View>
+      <Text style={styles.rate}>{vote_average}</Text>
+    </TouchableOpacity>
+  );
+};
---
+++ src/Components/MovieCard/styles.ts
@@ -0,0 +1,62 @@
+import {StyleSheet} from 'react-native';
+import {COLORS} from '../../Common/Colors';
+import {calcFont, calcHeight, calcWidth} from '../../Utils/responsive';
+
+export default StyleSheet.create({
+  container: {
+    width: '90%',
+    height: calcHeight(185),
+    borderRadius: calcWidth(20),
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: calcHeight(5),
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 1,
+    borderWidth: 0.5,
+    borderColor: COLORS.gray,
+    padding: calcWidth(15),
+    flexDirection: 'row',
+  },
+  image: {
+    width: calcWidth(100),
+    height: calcHeight(150),
+    borderRadius: calcWidth(10),
+  },
+  details: {
+    marginLeft: calcWidth(15),
+  },
+  title: {
+    fontSize: calcFont(20),
+    fontWeight: '700',
+    marginBottom: calcHeight(12),
+  },
+  date: {
+    fontSize: calcFont(14),
+    fontWeight: '600',
+  },
+  generView: {
+    width: calcWidth(75),
+    backgroundColor: COLORS.gray,
+    marginHorizontal: calcWidth(5),
+    height: calcHeight(25),
+    borderRadius: calcWidth(20),
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: calcWidth(4),
+    marginBottom: calcHeight(8),
+  },
+  gener: {
+    fontSize: calcFont(12),
+  },
+  rate: {
+    position: 'absolute',
+    bottom: calcHeight(20),
+    right: calcWidth(20),
+    fontSize: calcFont(20),
+    color: COLORS.green,
+    fontWeight: '800',
+  },
+});
