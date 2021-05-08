46dc86b5803230e8554818fb86547f504d44857d
MohamedHamdy2020 <MohamedHamdy2020@bitbucket.org>
2021-05-08 03:50:37 +0200

    add dummy_data

---
+++ src/Dummy_Data/Movies.ts
@@ -0,0 +1,44 @@
+export const cat_data = [
+  {id: '1', title: 'UpComing'},
+  {id: '2', title: 'Popular'},
+  {id: '3', title: 'Top Rated'},
+];
+
+export const movies_data = [
+  {
+    id: '1',
+    image: 'https://i.ytimg.com/vi/MJuFdpVCcsY/movieposter_en.jpg',
+    title: 'Title1',
+    release_date: '2021/5/8',
+    geners: [
+      {id: '1', title: 'Action'},
+      {id: '2', title: 'Science'},
+      {id: '3', title: 'Romance'},
+    ],
+    vote_average: '60%',
+  },
+  {
+    id: '2',
+    image: 'https://i.ytimg.com/vi/MJuFdpVCcsY/movieposter_en.jpg',
+    title: 'Title2',
+    release_date: '2021/5/8',
+    geners: [
+      {id: '1', title: 'Action'},
+      {id: '2', title: 'Science'},
+      {id: '3', title: 'Romance'},
+    ],
+    vote_average: '50%',
+  },
+  {
+    id: '3',
+    image: 'https://i.ytimg.com/vi/MJuFdpVCcsY/movieposter_en.jpg',
+    title: 'Title3',
+    release_date: '2021/5/8',
+    geners: [
+      {id: '1', title: 'Action'},
+      {id: '2', title: 'Science'},
+      {id: '3', title: 'Romance'},
+    ],
+    vote_average: '90%',
+  },
+];
