3f51f94cbafad3bc0f9c41b004b663e76613e0b8
MohamedHamdy2020 <MohamedHamdy2020@bitbucket.org>
2021-05-08 03:49:46 +0200

    connect App.tsx to redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import Store from './Store/store';
+
+import {MovieDetails} from './Screens/MovieDetails';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <Provider store={Store.store}>
+      <PersistGate loading={null} persistor={Store.persistor}>
+        <MovieDetails />
+      </PersistGate>
+    </Provider>
   );
 };
 
