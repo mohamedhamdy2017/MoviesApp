46cac85776c8000d27f87a66cc25e85d9dc41cd2
MohamedHamdy2020 <MohamedHamdy2020@bitbucket.org>
2021-05-09 23:07:45 +0200

    Add index & key props

--- src/Components/CategoryCard/index.tsx
+++ src/Components/CategoryCard/index.tsx
@@ -3,22 +3,25 @@ import {Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 
 interface CardProps {
+  index: number;
   title: String;
   onPress: () => void;
   selected: boolean;
 }
 
 export const CategoryCard: React.FC<CardProps> = ({
+  index,
   title,
   onPress,
   selected,
 }: CardProps) => {
   return (
     <TouchableOpacity
+      key={`item-${index.toString()}`}
       style={[styles.container, selected && styles.selection]}
       activeOpacity={0.7}
       onPress={onPress}>
-      <Text style={[styles.title,selected && styles.selection]}>{title}</Text>
+      <Text style={[styles.title, selected && styles.selection]}>{title}</Text>
     </TouchableOpacity>
   );
 };
