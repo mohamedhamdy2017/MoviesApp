aa3b1e71d7350d7a5496ed7ad331cd6b0f75664a
MohamedHamdy2020 <MohamedHamdy2020@bitbucket.org>
2021-05-09 23:13:56 +0200

    update movies list by filter it by categories

--- src/Screens/Movies/index.tsx
+++ src/Screens/Movies/index.tsx
@@ -1,20 +1,75 @@
-import React, {useState} from 'react';
-import {View, FlatList, Text, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  FlatList,
+  Text,
+  SafeAreaView,
+  ActivityIndicator,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import {MovieCard} from '../../Components/MovieCard';
 import {CategoryCard} from '../../Components/CategoryCard';
 import styles from './styles';
-import {cat_data, movies_data} from '../../Dummy_Data/Movies';
+import {cat_data} from '../../Dummy_Data/Movies';
+import {useDispatch, useSelector} from 'react-redux';
+import {get_genres, get_movies} from '../../Store/actions/MoviesActions';
+import {LoadingModal} from '../../Components/LoadingModal';
+
+interface ITEM {
+  poster_path: string;
+  vote_average: string;
+  title: string;
+  release_date: string;
+  genre_ids: Array<string>;
+}
+
+interface CATEGORYITEMPROPS {
+  title: string;
+}
 
 export const Movies: React.FC = () => {
   const navigation = useNavigation();
-  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(0);
+  const dispatch = useDispatch();
 
-  const renderHeaderItem = ({item, index}: {item: Object; index: number}) => {
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState<number>(0);
+  const [type, setType] = useState<string>('upcoming');
+
+  const {movies, page, loading} = useSelector((state: any) => {
+    return {
+      movies: state.movies.movies,
+      page: state.movies.page,
+      loading: state.movies.loading,
+    };
+  });
+
+  useEffect(() => {
+    const type =
+      selectedCategoryIndex == 0
+        ? 'upcoming'
+        : selectedCategoryIndex == 1
+        ? 'popular'
+        : 'top_rated';
+    setType(type);
+  }, [selectedCategoryIndex]);
+
+  useEffect(() => {
+    dispatch(get_movies({type, page: 1}));
+    dispatch(get_genres());
+  }, [selectedCategoryIndex]);
+
+  const renderHeaderItem = ({
+    item,
+    index,
+  }: {
+    item: CATEGORYITEMPROPS;
+    index: number;
+  }) => {
     const selected = selectedCategoryIndex == index;
+
     return (
       <CategoryCard
+        index={index}
         title={item.title}
         onPress={() => {
           setSelectedCategoryIndex(index);
@@ -34,21 +89,27 @@ export const Movies: React.FC = () => {
         renderItem={renderHeaderItem}
         horizontal
         showsHorizontalScrollIndicator={false}
+        initialNumToRender={8}
       />
     );
   };
 
-  const renderMoviesItem = ({item, index}: {item: Object; index: number}) => {
+  const renderMoviesItem = ({item, index}: {item: ITEM; index: number}) => {
     return (
       <MovieCard
-        image={{uri: item.image}}
+        index={index}
+        poster_path={item.poster_path}
         vote_average={item.vote_average}
         title={item.title}
         release_date={item.release_date}
-        geners={item.geners}
+        genre_ids={item.genre_ids}
         onPress={() => navigation.navigate('MovieDetails', {item})}
       />
     );
+  };
+
+  const onEndReached = () => {
+    dispatch(get_movies({type, page: page + 1}));
   };
 
   return (
@@ -57,11 +118,16 @@ export const Movies: React.FC = () => {
         <Text style={styles.head}>{'Movies'}</Text>
         {ListHeaderComponent()}
         <FlatList
-          data={movies_data}
+          contentContainerStyle={{paddingBottom: 120}}
+          data={movies}
           keyExtractor={(item, index) => index.toString()}
           renderItem={renderMoviesItem}
           showsVerticalScrollIndicator={false}
+          initialNumToRender={8}
+          onEndReached={onEndReached}
+          onEndReachedThreshold={0.5}
         />
+        <LoadingModal visible={loading} />
       </View>
     </SafeAreaView>
   );
