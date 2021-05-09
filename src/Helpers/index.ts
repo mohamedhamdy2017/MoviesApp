dfee143c6d245356cb3bd82a413d7bffe22ff859
MohamedHamdy2020 <MohamedHamdy2020@bitbucket.org>
2021-05-09 23:11:18 +0200

    Add helper file

---
+++ src/Helpers/index.ts
@@ -0,0 +1,4 @@
+export const getImagePath = (path: string) =>
+  `https://image.tmdb.org/t/p/w440_and_h660_face${path}`;
+export const getBGImagePath = (path: string) =>
+  `https://image.tmdb.org/t/p/w440_and_h660_face${path}`;
