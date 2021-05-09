d913a7328aa45ad1ebef626bf6da982d4a4e7e90
MohamedHamdy2020 <MohamedHamdy2020@bitbucket.org>
2021-05-09 23:14:16 +0200

    Add services

---
+++ src/Services/index.ts
@@ -0,0 +1,17 @@
+export const base_url = 'https://api.themoviedb.org/3';
+export const api_key = 'api_key=4f298a53e552283bee957836a529baec';
+
+type ARGUMENT = {
+  target: string;
+};
+
+export const get_request = async ({target}: ARGUMENT) => {
+  const url = `${base_url}/${target}`;
+  try {
+    const result = await fetch(url, {method: 'GET'});
+    return await result.json();
+  } catch (error) {
+    console.log('error', error);
+    return null;
+  }
+};
